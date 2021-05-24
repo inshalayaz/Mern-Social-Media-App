1dd13953f0312bf35998d66a6287210f2d1d2eb8
inshal ayaz <inshal.ayaz14@gmail.com>
2021-05-24 21:15:59 +0500

    Receive API Data from Heroku

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const url = 'https://inshal-mern-social-media-app.herokuapp.com/posts'
-
+ 
 export const fetchPost = () => axios.get(url)
 export const createPost = (newPost) => axios.post(url,newPost)
 export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost)
