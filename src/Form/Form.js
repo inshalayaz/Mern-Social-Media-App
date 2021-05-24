0ed4eb459d4ba0158e05b748929161d36848281e
inshal ayaz <inshal.ayaz14@gmail.com>
2021-05-24 18:48:35 +0500

    Frontend Completed

---
+++ src/Form/Form.js
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react'
+import useStyles from './Styles'
+import { Paper, TextField, Button, Typography } from '@material-ui/core'
+import FileBase64 from 'react-file-base64'
+import { useDispatch, useSelector } from 'react-redux'
+import { createPost,updatePost } from '../actions/posts'
+ 
+function Form({ currentId, setCurrentId }) {
+    const classes = useStyles();
+    const [postData, setPostData] = useState({ creator: '', title: '', message: '', tags: '', selectedFiles: '' })
+    const post = useSelector((state) => currentId ? state.posts.find((post)=> post._id === currentId ) : null )
+
+
+
+    useEffect(()=>{
+        if(post){
+            setPostData(post)
+        }
+    }, [post])
+    const dispatch = useDispatch()
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if(currentId){
+            dispatch(updatePost(currentId,postData))
+        }else{
+        dispatch(createPost(postData))
+    }
+    clear()
+}
+
+    const clear = () => {
+        setCurrentId(null)
+        setPostData({ creator: '', title: '', message: '', tags: '', selectedFiles: '' })
+    }
+    return (
+        <Paper className={ classes.paper }>
+            <form className= { `${classes.root}  ${classes.form}` } noValidate autoComplete='off' onSubmit={handleSubmit} >
+                <Typography variant='h6'> { currentId ? `Editing` : `Creating` } Memory </Typography>
+                <TextField  name='creator' variant='outlined' label='Creator' value={postData.creator} onChange={(e) => setPostData({ ...postData, creator:e.target.value })}/>
+                <TextField  name='title' variant='outlined' label='Title' value={postData.title} onChange={(e) => setPostData({ ...postData, title:e.target.value })}/>
+                <TextField  name='message' variant='outlined' label='Message' value={postData.message} onChange={(e) => setPostData({ ...postData, message:e.target.value })}/>
+                <TextField  name='tag' variant='outlined' label='Tag' value={postData.tags} onChange={(e) => setPostData({ ...postData, tags:e.target.value })}/>
+                <div className={classes.fileInput}>
+                    <FileBase64 type="file" multiple={false} onDone={({ base64 }) => setPostData({ ...postData, selectedFiles: base64 })} />
+                </div>
+                <Button className={classes.buttonSubmit} variant='contained' color='primary' size='large' type='submit' fullWidth > Submit </Button>
+                <Button  variant='contained' color='secondary' size='small' onClick={clear} fullWidth > Clear Form </Button>
+            </form>
+        </Paper>
+    )
+}
+
+export default Form
---
+++ src/Posts/Posts.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Post from './Post/Post'
+import useStyles from './Styles'
+import { Grid, CircularProgress } from '@material-ui/core'
+import { useSelector } from 'react-redux'
+function Posts({setCurrentId}) {
+    const classes = useStyles();
+    const posts = useSelector((state) => state.posts )
+    console.log(posts)
+    return (
+        <div>
+            {
+                !posts.length? <CircularProgress /> : 
+                <Grid>
+                    {
+                        posts.map( post => (
+                            <Grid key="_id" xs="12" item sm="6" >
+                                <Post post= {post} setCurrentId={setCurrentId} />
+                            </Grid>
+                        ))
+                    }
+                </Grid>
+            }
+        </div>
+    )
+}
+
+export default Posts
