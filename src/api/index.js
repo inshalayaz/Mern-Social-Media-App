c6704c1bf6aca280f5adcef03b81049641880595
inshal ayaz <inshal.ayaz14@gmail.com>
2021-06-01 19:44:23 +0500

    update

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
-const url = 'https://inshal-mern-social-media-app.herokuapp.com/posts'
+const API = axios.create({ baseURL: 'https://inshal-mern-social-media-app.herokuapp.com' })
  
-export const fetchPost = () => axios.get(url)
-export const createPost = (newPost) => axios.post(url,newPost)
-export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost)
-export const deletePost = (id) => axios.delete(`${url}/${id}`);
-export const likePost = (id) => axios.patch(`${url}/${id}/likePost`)
+export const fetchPost = () => API.get('./posts')
+export const createPost = (newPost) => API.post('/posts',newPost)
+export const updatePost = (id, updatedPost) => API.patch(`/posts/${id}`, updatedPost)
+export const deletePost = (id) => API.delete(`/posts/${id}`);
+export const likePost = (id) => API.patch(`/posts/${id}/likePost`)
